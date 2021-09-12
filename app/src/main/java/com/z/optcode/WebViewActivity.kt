1e235e24f3df65811eabf34ea8e672054f50ddeb
sink5 <harkben50@gmail.com>
2021-09-12 22:15:46 +0800

    JavascriptInterface 默认会忽略

--- app/src/main/java/com/z/optcode/WebViewActivity.kt
+++ app/src/main/java/com/z/optcode/WebViewActivity.kt
@@ -1,6 +1,7 @@
 package com.z.optcode
 
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import androidx.appcompat.app.AppCompatActivity
 import com.z.optcode.databinding.ActWebviewBinding
 
@@ -13,6 +14,16 @@ class WebViewActivity:AppCompatActivity() {
         setContentView(vb.root)
         vb.webView.settings.javaScriptEnabled = true
         vb.webView.loadUrl("https://www.baidu.com")
+        vb.webView.addJavascriptInterface(JSBridge(),"JSBridge")
 
     }
+
+    private class JSBridge{
+        @JavascriptInterface
+        fun log(){}
+
+        @JavascriptInterface
+        fun finish(){}
+    }
+
 }
