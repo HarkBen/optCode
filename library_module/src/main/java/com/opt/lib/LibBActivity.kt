864e387ebccba5d7f8177331fb26b9fa9d2a1912
sink5 <harkben50@gmail.com>
2021-09-12 23:17:38 +0800

    proguardFiles 测试结果

--- library_module/src/main/java/com/opt/lib/LibBActivity.kt
+++ library_module/src/main/java/com/opt/lib/LibBActivity.kt
@@ -2,7 +2,13 @@ package com.opt.lib
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.opt.lib.data.LibBInfo
+import com.opt.lib.reflection.JavaMessage
+import com.opt.lib.reflection.LibMessage
+import com.opt.lib.ui.LibBUIView
+import com.opt.lib.utils.LibBUtils
 
 class LibBActivity:Activity() {
 
@@ -11,5 +17,14 @@ class LibBActivity:Activity() {
         setContentView(TextView(this).apply {
             text = "LibBActivity"
         })
+        val view = LibBUIView(this,null)
+        "quote：${LibBInfo::class.java}".log()
+        "quote：${JavaMessage::class.java}".log()
+        "quote：${LibMessage::class.java}".log()
+        "quote：${LibBUtils::class.java}".log()
+
     }
+}
+private fun String.log(secondTag:String=""){
+    Log.i("proguard_log","$secondTag->$this")
 }
--- library_module/src/main/java/com/opt/lib/ui/LibBUIView.kt
+++ library_module/src/main/java/com/opt/lib/ui/LibBUIView.kt
@@ -1,4 +1,10 @@
 package com.opt.lib.ui
 
-class LibBUIView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class LibBUIView (context: Context, attributeSet: AttributeSet?)
+    : View(context,attributeSet) {
+
 }
